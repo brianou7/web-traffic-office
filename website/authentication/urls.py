9ae0f23decd20293aabcc7e4377892a389bb797a
Brian <brahianocampo7@gmail.com>
2025-03-26 21:16:41 -0500

    Add sign up view

--- website/authentication/urls.py
+++ website/authentication/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import AuthView, logout_view
+from .views import AuthView, SignUpView, logout_view
 
 
 app_name = 'auth'
 
 urlpatterns = [
     path("", AuthView.as_view(), name="index"),
+    path("sign-up", SignUpView.as_view(), name="sign-up"),
     path("logout", logout_view, name="logout")
 ]
--- website/authentication/views.py
+++ website/authentication/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.views import View
 
+from owners.models import Owner
+from owners.forms import OwnerForm
+
 
 class AuthView(View):
 
@@ -18,6 +21,31 @@ class AuthView(View):
             return redirect('violations:index')
 
         return render(request, 'auth/sign-in.html', {'errors': form.errors})
+
+
+class SignUpView(View):
+
+    context = {
+        'types': Owner.TYPES,
+        'id_types': Owner.ID_TYPES,
+    }
+
+    def get(self, request):
+        return render(request, 'auth/sign-up.html', self.context)
+
+    def post(self, request):
+        form = OwnerForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            user.set_password(form.cleaned_data['password'])
+            user.save(force_update=True)
+            login(request, user)
+            return redirect('violations:index')
+
+        context = self.context | {'errors': form.errors}
+        return render(request, 'auth/sign-up.html', context)
 
 
 def logout_view(request):
